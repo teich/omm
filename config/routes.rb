6adcd7cc80fbbf5b474d4ee9c8bcc4a53f08f05d
Oren Teich <oren@heroku.com>
2010-06-21 16:59:34 -0700

    default to messages

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Mailer::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "messages#index"
 
   # See how all your routes lay out with "rake routes"
 
